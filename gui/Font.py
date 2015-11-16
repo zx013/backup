51b7cbd41d1985bbffca2aa19bad2232f142efff
zx013 <1453145267@qq.com>
2015-11-16 13:50:15 +0800

    添加中文的显示

---
+++ gui/Font.py
@@ -0,0 +1,5 @@
+#-*- coding:utf-8 -*-
+import kivy
+
+kivy.resources.resource_add_path('C:/Windows/Fonts/')
+font = kivy.resources.resource_find('simkai.ttf')
--- gui/Main.py
+++ gui/Main.py
@@ -1,9 +1,17 @@
 #-*- coding:utf-8 -*-
+import kivy
+kivy.require('1.9.0')
+
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+
+class MyTextInput(TextInput):
+	pass
 
 class Main(App):
     def build(self):
-        return Button(text='Hello World')
+        return MyTextInput()
 
-Main().run()
+if __name__ == '__main__':
+	Main().run()
