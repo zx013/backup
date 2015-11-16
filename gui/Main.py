412e3dd82e9a25f024ca1fb988d76d1b44212139
zx013 <1453145267@qq.com>
2015-11-16 16:22:47 +0800

    简易的界面设计文档

--- gui/Main.py
+++ gui/Main.py
@@ -3,15 +3,19 @@ import kivy
 kivy.require('1.9.0')
 
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
 
 
 class MyTextInput(TextInput):
 	pass
 
+class MainWidget(Widget):
+	pass
+
 class Main(App):
     def build(self):
-        return MyTextInput()
+        return MainWidget()
 
 if __name__ == '__main__':
 	Main().run()
