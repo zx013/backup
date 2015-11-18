e196b905d6b57c52d3c8f421c0890e1c81d893e3
zx013 <1453145267@qq.com>
2015-11-18 16:45:43 +0800

    文件框

--- gui/FileList.py
+++ gui/FileList.py
@@ -5,19 +5,23 @@ kivy.require('1.9.0')
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.gridlayout import GridLayout
 
-from Tools import apply_walk
+from Tools import apply_walk, apply_args
 
 
 class BaseLabel(Label):
+	def __init__(self, **kwargs):
+		super(BaseLabel, self).__init__(**kwargs)
+
+	@apply_args
 	def update(self, **kwargs):
-		print kwargs
+		pass
 
 	def show(self):
 		pass
 
-class FileLabel(BoxLayout):
+class FileLabel(GridLayout):
 	def __init__(self, **kwargs):
 		super(FileLabel, self).__init__(**kwargs)
 		self.data = kwargs
@@ -33,17 +37,27 @@ class FileLabel(BoxLayout):
 	def show(self):
 		pass
 
-class FileList(RelativeLayout):
-	def refresh(self):
+class FileList(GridLayout):
+	def __init__(self, **kwargs):
+		super(FileList, self).__init__(**kwargs)
+		for i in xrange(kwargs.get('length', 0)):
+			filelabel = FileLabel(length=4)
+			self.add_widget(filelabel)
+
+	@apply_walk
+	def update(self, **kwargs):
 		pass
 
-	def update(self, filelist):
+	@apply_walk
+	def show(self):
 		pass
 
 class FileListApp(App):
 	def build(self):
-		f = FileLabel(length=4)
-		f.update(te=['a', 'b', 'c', 'd', 'e'], ls=range(5))
+		f = FileList(length=3)
+		f.update(text=[('a', 'b', 'c', 'd')] * len(f.children))
+		#f.update(size_hint_y=[(.1, .2, .3, .4)] * len(f.children))
+		f.update(size_hint_x=[(.2, .3, .1, .4)] * len(f.children))
 		return f
 
 if __name__ == '__main__':
--- gui/Tools.py
+++ gui/Tools.py
@@ -8,9 +8,19 @@ def unpack(**kwargs):
 def apply_walk(func):
 	@wraps(func)
 	def run(self, *args, **kwargs):
-			ret = func(self, *args, **kwargs)
-			for child, kwarg in zip(self.children, unpack(**kwargs)):
-				if getattr(child, func.__name__, None): #子控件中存在该方法
-					eval('child.%s' % func.__name__)(**kwarg)
-			return ret
+		ret = func(self, *args, **kwargs)
+		for child, kwarg in zip(self.children[::-1], unpack(**kwargs)): #children的顺序是相反的
+			if getattr(child, func.__name__, None): #子控件中存在该方法
+				eval('child.%s' % func.__name__)(**kwarg)
+		return ret
+	return run
+
+#将**kwargs中的数据转换为类中的属性
+def apply_args(func):
+	@wraps(func)
+	def run(self, *args, **kwargs):
+		ret = func(self, *args, **kwargs)
+		for key, value in kwargs.items():
+				setattr(self, key, value)
+		return ret
 	return run
