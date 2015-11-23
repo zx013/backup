ea8620ae76653ff5e98b45b1de346b178fa7e5ac
zx013 <1453145267@qq.com>
2015-11-23 15:55:05 +0800

    添加删除事件

--- gui/FileList.py
+++ gui/FileList.py
@@ -6,15 +6,20 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 
-from Tools import apply_insert, apply_walk, apply_args
+from kivy.logger import Logger
+
+from Tools import apply_insert, apply_delete, apply_walk, insert_args, delete_args
 
 
 class BaseLabel(Label):
 	def insert(self, **kwargs):
-		apply_args(self, **kwargs)
+		insert_args(self, **kwargs)
+
+	def delete(self, **kwargs):
+		delete_args(self, **kwargs)
 
 	def update(self, **kwargs):
-		apply_args(self, **kwargs)
+		insert_args(self, **kwargs)
 
 	def show(self):
 		pass
@@ -22,6 +27,10 @@ class BaseLabel(Label):
 class FileLabel(GridLayout):
 	@apply_insert(BaseLabel)
 	def insert(self, **kwargs):
+		pass
+
+	@apply_delete
+	def delete(self, **kwargs):
 		pass
 
 	@apply_walk
@@ -37,6 +46,10 @@ class FileList(GridLayout):
 	def insert(self, **kwargs):
 		pass
 
+	@apply_delete
+	def delete(self, **kwargs):
+		pass
+
 	@apply_walk
 	def update(self, **kwargs):
 		pass
@@ -49,9 +62,10 @@ class FileListApp(App):
 	def build(self):
 		f = FileList()
 		f.insert(a=[range(4)] * 3)
-		f.update(text=[['a', 'b', 'c', 'd']] * len(f.children))
+		f.update(text=[['a', 'b', 'a', 'd', 'ab']] * len(f.children))
 		#f.update(size_hint_y=[[.1, .2, .3, .4]] * len(f.children))
 		f.update(size_hint_x=[[.2, .3, .1, .4]] * len(f.children))
+		f.delete(text=[[('a', 'b'), ('a', 'c'), 'd', 'e']] * len(f.children))
 		return f
 
 if __name__ == '__main__':
--- gui/Tools.py
+++ gui/Tools.py
@@ -10,7 +10,7 @@ def apply_insert(widget_class):
 		@wraps(func)
 		def run(self, *args, **kwargs):
 			ret = func(self, *args, **kwargs)
-			for kwarg in unpack(**kwargs): #根据提供的参数插入子控件
+			for kwarg in unpack(**kwargs): #根据提供的参数插入子节点
 				widget = widget_class()
 				self.add_widget(widget)
 				widget_func = getattr(widget, func.__name__, None)
@@ -19,6 +19,26 @@ def apply_insert(widget_class):
 			return ret
 		return run
 	return run_func
+
+#提供的参数少于节点数，将最后一个参数应用到所有节点
+#提供的参数多于节点数，将最后一个参数应用到当前节点
+def apply_delete(func):
+	@wraps(func)
+	def run(self, *args, **kwargs):
+		ret = func(self, *args, **kwargs)
+		unpack_child = self.children[::-1]
+		unpack_kwargs = unpack(**kwargs)
+		if len(unpack_kwargs) > len(unpack_child):
+			delete_args(self, **unpack_kwargs[-1])
+		else:
+			if len(unpack_kwargs) < len(unpack_child):
+				unpack_kwargs += [unpack_kwargs[-1]] * (len(unpack_child) - len(unpack_kwargs))
+			for child, kwarg in zip(unpack_child, unpack_kwargs): #children的顺序是相反的
+				child_func = getattr(child, func.__name__, None)
+				if child_func: #子控件中存在该方法
+					child_func(**kwarg)
+		return ret
+	return run
 
 
 #将**kwargs依次应用到children中
@@ -29,7 +49,7 @@ def apply_walk(func):
 		unpack_child = self.children[::-1]
 		unpack_kwargs = unpack(**kwargs)
 		if len(unpack_kwargs) > len(unpack_child):
-			apply_args(self, **unpack_kwargs[-1])
+			insert_args(self, **unpack_kwargs[-1])
 		for child, kwarg in zip(unpack_child, unpack_kwargs): #children的顺序是相反的
 			child_func = getattr(child, func.__name__, None)
 			if child_func: #子控件中存在该方法
@@ -38,6 +58,16 @@ def apply_walk(func):
 	return run
 
 #将**kwargs中的数据转换为类中的属性
-def apply_args(self, **kwargs):
+def insert_args(self, **kwargs):
 	for key, value in kwargs.items():
 		setattr(self, key, value)
+
+#属性和键值相等或属性在键值中时移除该节点
+def delete_args(self, **kwargs):
+	for key, value in kwargs.items():
+		try:
+			if getattr(self, key) == value or getattr(self, key) in value:
+				self.parent.remove_widget(self)
+				return
+		except:
+			pass
