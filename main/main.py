78743b1a0d6a0b3cac83fe384f1ea86491efcc19
zx013 <1453145267@qq.com>
2015-12-24 15:44:18 +0800

    文件列表显示为备份的文件或目录

--- main/main.py
+++ main/main.py
@@ -6,6 +6,7 @@ os.chdir(path)
 os.chdir('..')
 sys.path.append('.') #添加到搜索路径
 
+
 import kivy
 kivy.require('1.9.0')
 
@@ -15,15 +16,18 @@ from kivy.logger import Logger
 from gui.filemanager import FileManager
 from right_click import text, event
 
-import os
-from backup.disk import Disk
+from backup.config import Config
 
-def get_filelist(target_path):
+def get_filelist():
+	config = Config()
+	config.read_config()
+	filelist = config.config['backup'].keys()
+
 	t = []
-	target_dir, target_file = Disk.show(target_path)
-	for f in target_dir + target_file:
-		f_stat= os.stat(f)
-		t.append(map(str, [f.encode('utf-8'), f_stat.st_mtime, f_stat.st_dev, f_stat.st_size]))
+	for f in filelist:
+		if os.path.exists(f):
+			f_stat= os.stat(f)
+			t.append(map(str, [f.encode('utf-8'), f_stat.st_mtime, f_stat.st_dev, f_stat.st_size]))
 	return t
 
 def get_titlelabel():
@@ -33,7 +37,7 @@ def get_titlelabel():
 class DisplayScreen(FileManager):
 	w = [180, 180, 60, 40]
 	def build(self):
-		self.filelist.insert(text=get_filelist(u'C:/Users/Administrator/Desktop'))
+		self.filelist.insert(text=get_filelist())
 		self.filelist.update(width=[self.w] * len(self.filelist.children))
 
 		self.titlelabel.insert(text=get_titlelabel())
