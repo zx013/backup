da21b13407c300df6ce3b8b62f65c332b70e2f45
zx013 <1453145267@qq.com>
2015-12-30 16:46:47 +0800

    初始化模块

---
+++ event/init_event.py
@@ -0,0 +1,21 @@
+#-*- coding:utf-8 -*-
+import os
+import sys
+path = os.path.split(os.path.realpath(sys.argv[0]))[0]
+os.chdir(path)
+sys.path.append('.') #Ìí¼Óµ½ËÑË÷Â·¾¶
+
+from event import signal, connect
+import clickmenu_event
+import filelist_event
+import titlelabel_event
+import statusbar_event
+
+
+def system_init(*args, **kwargs):
+	self = args[0]
+	signal('clickmenu_init', self)
+	signal('filelist_init', self)
+	signal('titlelabel_init', self)
+	signal('statusbar_init', self)
+connect('system_init', system_init)
--- main.py
+++ main.py
@@ -1,30 +1,16 @@
 #-*- coding:utf-8 -*-
-import os
-import sys
-path = os.path.split(os.path.realpath(sys.argv[0]))[0]
-os.chdir(path)
-sys.path.append('.') #添加到搜索路径
-
-
 import kivy
 kivy.require('1.9.0')
 
 from kivy.app import App
 
 from gui.filemanager import FileManager
-import event.clickmenu_event
-import event.filelist_event
-import event.titlelabel_event
-import event.statusbar_event
-from event.event import signal
+from event import init_event, event
 
 
 class DisplayScreen(FileManager):
 	def build(self):
-		signal('clickmenu_init', self)
-		signal('filelist_init', self)
-		signal('titlelabel_init', self)
-		signal('statusbar_init', self)
+		event.signal('system_init', self)
 
 
 class mainApp(App):
